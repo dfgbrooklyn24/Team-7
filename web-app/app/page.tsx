eee565523317fbe021eba64f1409b9ef9180ae16
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2024-04-19 20:58:06 -0400

    Installed maps

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps"
+
+const geoUrl =
+  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
 
 export default function Home() {
   return (
@@ -29,7 +33,26 @@ export default function Home() {
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-              
+      import React from "react"
+import { ComposableMap, Geographies, Geography } from "react-simple-maps"
+
+const geoUrl =
+  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
+
+export default function MapChart() {
+  return (
+    <ComposableMap>
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+    </ComposableMap>
+  )
+}
+
       </div>
     </main>
   );
