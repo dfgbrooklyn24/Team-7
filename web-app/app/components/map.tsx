5db1517698445417b679a07f25c7d101b8657818
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2024-04-20 04:18:28 -0400

    marked neighbouring zip codes

--- web-app/app/components/map.tsx
+++ web-app/app/components/map.tsx
@@ -8,16 +8,27 @@ export function MyMap() {
   const [hue, setHue] = useState(0)
   const color = `hsl(${hue % 360}deg 39% 70%)`
   return (
-    <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+    <Map height={300} defaultCenter={[40.0150, -75.1472]} defaultZoom={11}>
         
-      <Marker width={50} anchor={[50.879, 4.6997]} />
+      <Marker width={50} anchor={[40.01445,-75.18663]} />
+      <Marker width={50} anchor={[39.99646,-75.17241]} />
+      <Marker width={50} anchor={[40.03166,-75.17983]} />
+      <Marker width={50} anchor={[40.03722,-75.14702]} />
+      <Marker width={50} anchor={[40.03331,-75.11749]} />
+      <Marker width={50} anchor={[40.01708,-75.09426]} />
       
       <Marker 
         width={50}
-        anchor={[50.875, 4.6997]} 
+        anchor={[40.0150, -75.1472]} 
         color={color} 
         onClick={() => setHue(hue + 20)} 
       />
     </Map>
   )
 }
+// 40.01445,-75.18663
+// 39.99646,-75.17241
+// 40.03166,-75.17983
+// 40.03722,-75.14702
+// 40.03331,-75.11749
+// 40.01708,-75.09426
